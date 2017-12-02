25d382f89f39ca75aa9605341215fbc212150a13
Reuben Raj <reubenraj@gmail.com>
2017-12-02 09:20:19 +0530

    updated files by reuben.

--- about.php
+++ about.php
@@ -1,4 +1,5 @@
 <?php
+$title = 'About Us';
 include 'webparts/topheader.php';
 ?>
 
@@ -28,9 +29,9 @@ include 'webparts/topheader.php';
 
 
         <section>
-            <div >
-                <div >
-                    <img src="img/medical.jpg" alt="">
+            <div class="row" >
+                <div class="col-md-12" >
+                    <img class="img-responsive" src="img/banners/aboutus.jpg" alt="about us">
                 </div>
 
 
@@ -48,7 +49,7 @@ include 'webparts/topheader.php';
 
 <div id="content" class="team-member-detail">
     <div class="container">
-        
+
         <section>
             <div class="row">
                 <div class="col-md-8">
@@ -56,10 +57,10 @@ include 'webparts/topheader.php';
                         <h3>About Founder of Change Gospel Ministries India </h3>
                     </div>
                     <p class="lead">We heartly welcome you to our Change Gospel Ministries in Jesus name. Myself Servant K.SatishBabu founder and My Beloved wife 
-                            K.V. Priyadarshini is the President of Gracious Children Home Ministries in India, Giving the Logistic support to the community to 
-                            create awareness on Various Community development and capacity building programs, with the help of  Government and non- government  
-                            organisations. Through these activites at the gross root level, we have access with the communities of different caste,creed and religion to 
-                            bring them under the wings of the Lord and many non christians are  coming into the Lord.</p>
+                        K.V. Priyadarshini is the President of Gracious Children Home Ministries in India, Giving the Logistic support to the community to 
+                        create awareness on Various Community development and capacity building programs, with the help of  Government and non- government  
+                        organisations. Through these activites at the gross root level, we have access with the communities of different caste,creed and religion to 
+                        bring them under the wings of the Lord and many non christians are  coming into the Lord.</p>
                 </div>
                 <div class="col-md-4">
                     <p class="text-center">
@@ -76,17 +77,17 @@ include 'webparts/topheader.php';
                         <h3>Our Mission</h3>
                     </div>
                     <p class="lead">
-                        
-                            To break the cycle of Poverty by providing holistic development to impoverish orphans and communications in India, through meeting
-                            their physical , educational, spirtual and socail needs. To change and equip children to live productive and responsible lives.
-                            The Gracious children ministries is a multi faceted ministry, addressing poverty stricken orgpans and communities through multiple initiatives.
-                            Primarily, this project benefits orphaned, abandoned and abused children, while offering programs and services to the community. By 
-                            taking the poores and most disadvanataged segment of society and giving them the love , care and an enivronment to achieve excellence, a significant
-                            impact is being made.As we raise these children with elevated standards and expectations, they become the top segment of the same society
-                            form which they came.
-                            No one has yet fully realized the wealth of sympathy , kindness and generosity hidden in the soul of a child. The effort of every true education should be to 
-                            unlock that treasure.
-                     
+
+                        To break the cycle of Poverty by providing holistic development to impoverish orphans and communications in India, through meeting
+                        their physical , educational, spirtual and socail needs. To change and equip children to live productive and responsible lives.
+                        The Gracious children ministries is a multi faceted ministry, addressing poverty stricken orgpans and communities through multiple initiatives.
+                        Primarily, this project benefits orphaned, abandoned and abused children, while offering programs and services to the community. By 
+                        taking the poores and most disadvanataged segment of society and giving them the love , care and an enivronment to achieve excellence, a significant
+                        impact is being made.As we raise these children with elevated standards and expectations, they become the top segment of the same society
+                        form which they came.
+                        No one has yet fully realized the wealth of sympathy , kindness and generosity hidden in the soul of a child. The effort of every true education should be to 
+                        unlock that treasure.
+
                     </p>
 
 
@@ -101,81 +102,81 @@ include 'webparts/topheader.php';
                         <h3> The Story of Gracious Childrens Home :</h3>
                     </div>
                     <p class="lead">
-                    
 
-                            Change India Gospel Ministries Provides ongoing assistance to the orphans, the poor, the widows and the elderly as well as 
-                            during the times of emergencies, such as the floods that occur during the rainy seasons in SouthIndia. With the help of the Lord
-                            Jesus christ, we are helping a lot of people in India and proclaim the word of Gospel to many people in India. we ask you to visit us and help
-                            and preach the Gospel. 
-                            Gracious Children's Home and Evangelist Satishbabu havebeen reaching out to the impoverished ,abandoned and orphaned children in the area of Yeleswaram, India Since
-                            2007 it will be prayerfully started by my mother Eswari.</p> <br> 
 
-                    <p class="lead">
-                       
-                            The Beginning of Gracious children's home actually started with gospel prayer ministries outreaches to the remote villages surrounding yeleswaram. 
-                            we are going door to door slum villages and seeing many people coming to the Lord. we praised the lord a lot about that! we encouraged them to attend our church
-                            and they were so happy . when we asked them about their lives, they explained to us about every thing.They would share about their situations and 
-                            give testimonies daily when we visited them .
-                        </p><br>
-                    <p class="lead">
-                      
-                            some shared how , in order to sustain their families, they had to collect things to sell from the garbage.
-                            In an effort to support their familes , they would also take their children with them and find them jobs as child laborers at rice mills and
-                            factories. Sometimes they spent days starving, having no food at all. we are prayerfully distributing rice, cooking oils and dal in the tribal
-                            slum people villages and some tribal villages have no pure waterm we many children are suffering health diseases due to impure water.
-                            we will establish a pure water bore wells in some villages, here me and my mother daily work and feed our orphanage children.
-                            please pray for our children.
-                        </p> <br>
+                        Change India Gospel Ministries Provides ongoing assistance to the orphans, the poor, the widows and the elderly as well as 
+                        during the times of emergencies, such as the floods that occur during the rainy seasons in SouthIndia. With the help of the Lord
+                        Jesus christ, we are helping a lot of people in India and proclaim the word of Gospel to many people in India. we ask you to visit us and help
+                        and preach the Gospel. 
+                        Gracious Children's Home and Evangelist Satishbabu havebeen reaching out to the impoverished ,abandoned and orphaned children in the area of Yeleswaram, India Since
+                        2007 it will be prayerfully started by my mother Eswari.</p> <br> 
 
                     <p class="lead">
-                      we saw these situtations and felt so sorry and we prayed for them, but we did not know what we could do to help .as we
-                            prayed each day for god to help these people,he spoke to us about it through his word in James. </p> <br>
+
+                        The Beginning of Gracious children's home actually started with gospel prayer ministries outreaches to the remote villages surrounding yeleswaram. 
+                        we are going door to door slum villages and seeing many people coming to the Lord. we praised the lord a lot about that! we encouraged them to attend our church
+                        and they were so happy . when we asked them about their lives, they explained to us about every thing.They would share about their situations and 
+                        give testimonies daily when we visited them .
+                    </p><br>
+                    <p class="lead">
+
+                        some shared how , in order to sustain their families, they had to collect things to sell from the garbage.
+                        In an effort to support their familes , they would also take their children with them and find them jobs as child laborers at rice mills and
+                        factories. Sometimes they spent days starving, having no food at all. we are prayerfully distributing rice, cooking oils and dal in the tribal
+                        slum people villages and some tribal villages have no pure waterm we many children are suffering health diseases due to impure water.
+                        we will establish a pure water bore wells in some villages, here me and my mother daily work and feed our orphanage children.
+                        please pray for our children.
+                    </p> <br>
+
+                    <p class="lead">
+                        we saw these situtations and felt so sorry and we prayed for them, but we did not know what we could do to help .as we
+                        prayed each day for god to help these people,he spoke to us about it through his word in James. </p> <br>
 
 
                     <li class="text-primary lead fa fa-caret-square-o-down text-center"> "Religion that God our Father accepts as pure and faultless is this: to look after orphans and widows in their distress and to 
                         keep oneself from being polluted by the world." James 1:27 </li>
 
                     <p class="lead ">
-                      we began to pray a lot for God to help us to establish a home for those children who were orphaned or abandoned or whose parenrs did not have 
-                            the means to care for them.Some people in the village supported us and encouraged us to start this precious ministry. we knew  we could
-                            not do it without God's provision.God Strengthened and encouraged us and we were led to beging with only 5 girls children in the year 2008.
-                            we just kept our hopes and trust in God only. God Spoke to my Mother Eswari, that the borders of our ministry were going to expand in miraculous
-                            ways.</p> <br>
+                        we began to pray a lot for God to help us to establish a home for those children who were orphaned or abandoned or whose parenrs did not have 
+                        the means to care for them.Some people in the village supported us and encouraged us to start this precious ministry. we knew  we could
+                        not do it without God's provision.God Strengthened and encouraged us and we were led to beging with only 5 girls children in the year 2008.
+                        we just kept our hopes and trust in God only. God Spoke to my Mother Eswari, that the borders of our ministry were going to expand in miraculous
+                        ways.</p> <br>
 
                     <p class="lead">
                         When we started Gracious Children's home in 2008, many people were so happy and praised God a lot.Even so, others did not like our idea.
-                            It has not always been easy to provide sufficent meals for the children.we faced many problems,not the least of which were a lack of food
-                            and being criticized for what er were trying to do.It was very difficult in those days.Yet we knew that God had called us to do this and that 
-                            he was able to provide for us.we put faith in Him in every way for our ministry. He called us to do this so we knew that he would equip us!
-                     
+                        It has not always been easy to provide sufficent meals for the children.we faced many problems,not the least of which were a lack of food
+                        and being criticized for what er were trying to do.It was very difficult in those days.Yet we knew that God had called us to do this and that 
+                        he was able to provide for us.we put faith in Him in every way for our ministry. He called us to do this so we knew that he would equip us!
+
                     </p><br>
                     <p class="lead ">
                         since that time, we have been able to bring a total of 26 children to gracious children home.The first girl we brought, Navya, has passed
-                            her exams and moved on to a Tailoring Training Programs.God has been so faithful to us  in every situation.
+                        her exams and moved on to a Tailoring Training Programs.God has been so faithful to us  in every situation.
 
-                            Now our Gracious children Home is being shaped in wonderful ways with some goood facilities. we do have more needs but we know that
-                            we can trust god to meet them at the right time.</p> <br>
-
-                    <p class="lead">
-                          we thank God that we now have many children who are getting good food and being educated,but more than that, they are growing in faith.our 
-                            hope is that our children would have such faith that it will let them forget what situations they are in and just praise God! our vision 
-                            is to make gracious Children's home a good place that always shows the Love of Christ. </p> <br>
+                        Now our Gracious children Home is being shaped in wonderful ways with some goood facilities. we do have more needs but we know that
+                        we can trust god to meet them at the right time.</p> <br>
 
                     <p class="lead">
-                         If this testimony has inpired you to partner with Satish Babu and Gracious Children's home or to sponser a child at GCH,you may give your
-                            tax-deductible gift at paypal.com by typing in **email** or by mailing your check or money order or western union, made out to
-                            to :Change Gospel Ministries India(CGM India )</p> <br>
+                        we thank God that we now have many children who are getting good food and being educated,but more than that, they are growing in faith.our 
+                        hope is that our children would have such faith that it will let them forget what situations they are in and just praise God! our vision 
+                        is to make gracious Children's home a good place that always shows the Love of Christ. </p> <br>
+
+                    <p class="lead">
+                        If this testimony has inpired you to partner with Satish Babu and Gracious Children's home or to sponser a child at GCH,you may give your
+                        tax-deductible gift at paypal.com by typing in **email** or by mailing your check or money order or western union, made out to
+                        to :Change Gospel Ministries India(CGM India )</p> <br>
                     <p class="text-center lead"> 
-                     Evangelist KANNURI SATISH BABU. 
-                            5-342/1,Subbarao peta,
-                            Katheru Post,
-                            Rajahmundry 533105
-                            East Godavari District
-                            Andhra Pradesh
-                            India.
+                        Evangelist KANNURI SATISH BABU. 
+                        5-342/1,Subbarao peta,
+                        Katheru Post,
+                        Rajahmundry 533105
+                        East Godavari District
+                        Andhra Pradesh
+                        India.
 
-                            Contact No : +91-9491689871,
-                            +91-9059111210 </p>
+                        Contact No : +91-9491689871,
+                        +91-9059111210 </p>
 
                     <p class=" col-md-12 lead text-center text-primary"> <strong>  May God bless you Richly and may his aboundng lover overwhelm you with unspeakable Joy! </strong></p>
 
--- webparts/head.php
+++ webparts/head.php
@@ -1,77 +1,85 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="robots" content="all,follow">
-    <meta name="googlebot" content="index,follow,snippet,archive">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta name="robots" content="all,follow">
+        <meta name="googlebot" content="index,follow,snippet,archive">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>CGM</title>
+        <title>
+            <?php
+            if (isset($title)) {
+                echo('Change Gospel Ministries India - ' . $title);
+            } else {
+                echo('Change Gospel Ministries India');
+            }
+            ?>
+        </title>
 
-    <meta name="keywords" content="">
+        <meta name="keywords" content="">
 
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,500,700,800' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,500,700,800' rel='stylesheet' type='text/css'>
 
-    <!-- Bootstrap and Font Awesome css -->
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+        <!-- Bootstrap and Font Awesome css -->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 
-    <!-- Css animations  -->
-    <link href="css/animate.css" rel="stylesheet">
+        <!-- Css animations  -->
+        <link href="css/animate.css" rel="stylesheet">
 
-    <!-- Theme stylesheet, if possible do not edit this stylesheet -->
-    <link href="css/style.default.css" rel="stylesheet" id="theme-stylesheet">
+        <!-- Theme stylesheet, if possible do not edit this stylesheet -->
+        <link href="css/style.default.css" rel="stylesheet" id="theme-stylesheet">
 
-    <!-- Custom stylesheet - for your changes -->
-    <link href="css/custom.css" rel="stylesheet">
+        <!-- Custom stylesheet - for your changes -->
+        <link href="css/custom.css" rel="stylesheet">
 
-    <!-- Responsivity for older IE -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
+        <!-- Responsivity for older IE -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
-    <!-- Favicon and apple touch icons-->
-    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
-    <link rel="apple-touch-icon" href="img/apple-touch-icon.png" />
-    <link rel="apple-touch-icon" sizes="57x57" href="img/apple-touch-icon-57x57.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png" />
-    <link rel="apple-touch-icon" sizes="76x76" href="img/apple-touch-icon-76x76.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png" />
-    <link rel="apple-touch-icon" sizes="120x120" href="img/apple-touch-icon-120x120.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="img/apple-touch-icon-144x144.png" />
-    <link rel="apple-touch-icon" sizes="152x152" href="img/apple-touch-icon-152x152.png" />
-    <!-- owl carousel css -->
+        <!-- Favicon and apple touch icons-->
+        <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="img/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="57x57" href="img/apple-touch-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="img/apple-touch-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="img/apple-touch-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="img/apple-touch-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="img/apple-touch-icon-152x152.png" />
+        <!-- owl carousel css -->
 
-    <link href="css/owl.carousel.css" rel="stylesheet">
-    <link href="css/owl.theme.css" rel="stylesheet">
-</head>
+        <link href="css/owl.carousel.css" rel="stylesheet">
+        <link href="css/owl.theme.css" rel="stylesheet">
+    </head>
 
-<body>
-    
+    <body>
 
-            <!-- *** TOP ***
+
+        <!-- *** TOP ***
 _________________________________________________________ -->
-            <div id="top">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xs-5 contact">
-                            <p class="hidden-sm hidden-xs">Contact us on +91-9059111210 or changegospelministries@gmail.com</p>
-                            <p class="hidden-md hidden-lg"><a href="#" data-animate-hover="pulse"><i class="fa fa-phone"></i></a>  <a href="#" data-animate-hover="pulse"><i class="fa fa-envelope"></i></a>
-                            </p>
+        <div id="top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-5 contact">
+                        <p class="hidden-sm hidden-xs">Contact us on +91-9059111210 or changegospelministries@gmail.com</p>
+                        <p class="hidden-md hidden-lg"><a href="#" data-animate-hover="pulse"><i class="fa fa-phone"></i></a>  <a href="#" data-animate-hover="pulse"><i class="fa fa-envelope"></i></a>
+                        </p>
+                    </div>
+                    <div class="col-xs-7">
+                        <div class="social">
+                            <a href="#" class="external facebook" data-animate-hover="pulse"><i class="fa fa-facebook"></i></a>
+                            <a href="#" class="external gplus" data-animate-hover="pulse"><i class="fa fa-google-plus"></i></a>
+                            <a href="#" class="external twitter" data-animate-hover="pulse"><i class="fa fa-twitter"></i></a>
+                            <a href="contact.php" class="email" data-animate-hover="pulse"><i class="fa fa-envelope"></i></a>
                         </div>
-                        <div class="col-xs-7">
-                            <div class="social">
-                                <a href="#" class="external facebook" data-animate-hover="pulse"><i class="fa fa-facebook"></i></a>
-                                <a href="#" class="external gplus" data-animate-hover="pulse"><i class="fa fa-google-plus"></i></a>
-                                <a href="#" class="external twitter" data-animate-hover="pulse"><i class="fa fa-twitter"></i></a>
-                                <a href="contact.php" class="email" data-animate-hover="pulse"><i class="fa fa-envelope"></i></a>
-                            </div>
 
-                          
 
-                        </div>
+
                     </div>
                 </div>
             </div>
+        </div>
